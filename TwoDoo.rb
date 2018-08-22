f592d88a4c316595557644de9c145cfae64d44b2
Bijan Ghasemi Afshar <bijan.ghasemiafshar@gmail.com>
2018-08-22 21:28:44 +0100

    Error handling + more instanse methods

--- TwoDoo.rb
+++ TwoDoo.rb
@@ -2,39 +2,92 @@ module TwoDoo
 
   class Task 
 
-  	@@number_of_tasks
+  	@@number_of_tasks = 0
 
-  	attr_accessor :title, :description, :start_date, :end_date, :label, :finished_date
+  	attr_accessor :title, :description, :start_date, :end_date, :label, :finished_date    
 
-    def initialize(title='task1', description='', start_date=Time.new, end_date=(Time.new + 30), label='')
+    def initialize(title, description, start_date, end_date, label)
 
-			@title = title
-			@description = description
-			@start_date = start_date
-			@end_date = end_date
-			@label = label
+      if(validate_mandatory_input(title, end_date))
+
+        @id = title.crypt(end_date.to_s)
+        @title = title
+        @description = description
+        @start_date = start_date
+        @end_date = end_date
+        @label = label
+        @@number_of_tasks += 1        
+
+      else
+
+        raise "Title & End date are mandatory"
+
+      end      
+
+    end        
+
+    def self.number_of_tasks
+
+      @@number_of_tasks
 
     end
 
     def to_s
-    	"title: #{title}\ndescription: #{description}\nstart_date: #{start_date}\nend_date: #{end_date}\nlabel: #{label}"
+
+    	"ID: #{@id}\nTitle: #{@title}\nDescription: #{@description}\nStart date: #{@start_date}\nEnd date: #{@end_date}\nLabel: #{@label}\n"
+
+    end
+
+    private
+
+    def validate_mandatory_input(title, end_date)
+      
+      !title.to_s.empty? and !end_date.to_s.empty? and end_date.is_a?(Time)
+
     end
 
   end
+  private_constant :Task
+
+
 
   class List
 
   	def initialize
   		
-  		@list_of_tasks.new
+  		@list_of_tasks = Array.new
 
   	end
 
-  	def add_task
+  	def add_task(title, description, start_date, end_date, label)
 
-  		@list_of_tasks.push(Task.new())
+  		@list_of_tasks.push(Task.new(title, description, start_date, end_date, label))
   		
   	end
+
+    def remove_task
+      
+      # To be implemented
+
+    end
+
+    def finished_task
+      
+    end
+
+    def send_notification
+      
+    end
+
+    def to_s
+
+      rep = "____List____ (#{Task.number_of_tasks} tasks)\n"
+      @list_of_tasks.each do |task|
+        rep.concat("#{task}\n")
+      end
+      return rep
+
+    end
 
   end
 
--- main.rb
+++ main.rb
@@ -2,6 +2,17 @@ $LOAD_PATH << '.'
 
 require 'TwoDoo'
 
-task_1 = TwoDoo::Task.new('New Task 01', 'This is for testing purposes', nil, nil, 'Test')
+list = TwoDoo::List.new
 
-puts "Task_1: \n#{task_1}"
+begin
+	
+	list.add_task('New Task 01', 'This is for testing purposes 01', nil, Time.new, 'Test_01')
+	# list.add_task('New Task 02', 'This is for testing purposes 02', nil, nil, 'Test_02')
+	puts list
+
+rescue Exception => e
+	
+	puts "______________There was an error______________\n#{e.message}\n#{e.backtrace.inspect}"
+	# retry
+
+end
